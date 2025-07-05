a149bed6272c66fc5d501a6683bc2672d0ea3171
aimanyusufw <aimanyusufdev@gmail.com>
2025-07-05 13:03:02 +0700

    feat : add curator media library and stephenjude/blogtamplate

--- app/Providers/Filament/AdminPanelPanelProvider.php
+++ app/Providers/Filament/AdminPanelPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Awcodes\Curator\CuratorPlugin;
 use Filament\Http\Middleware\Authenticate;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -18,6 +19,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Stephenjude\FilamentBlog\BlogPlugin;
 
 class AdminPanelPanelProvider extends PanelProvider
 {
@@ -41,6 +43,7 @@ class AdminPanelPanelProvider extends PanelProvider
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
             ])
+            ->theme(asset('css/filament/admin-panel/theme.css'))
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -54,6 +57,14 @@ class AdminPanelPanelProvider extends PanelProvider
             ])
             ->plugins([
                 FilamentShieldPlugin::make(),
+                BlogPlugin::make(),
+                CuratorPlugin::make()
+                    ->label('Media')
+                    ->pluralLabel('Media')
+                    ->navigationIcon('heroicon-o-photo')
+                    ->navigationGroup('Content')
+                    ->registerNavigation(true)
+                    ->defaultListView('grid')
             ])
             ->authMiddleware([
                 Authenticate::class,
