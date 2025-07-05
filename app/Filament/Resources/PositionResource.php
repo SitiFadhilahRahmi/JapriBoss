97e0e4c1cb34b42311858c7090a2df8e9ddb4673
aimanyusufw <aimanyusufdev@gmail.com>
2025-07-05 16:26:09 +0700

    feat : add posisition resource

---
+++ app/Filament/Resources/PositionResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PositionResource\Pages;
+use App\Filament\Resources\PositionResource\RelationManagers;
+use App\Models\Position;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PositionResource extends Resource
+{
+    protected static ?string $model = Position::class;
+
+    protected static ?string $navigationGroup = 'Our Team';
+
+    protected static ?string $navigationIcon = 'heroicon-o-star';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label("Position Name")
+                    ->helperText("e.g Manager, Director, Supervisor")
+                    ->placeholder("Supervison")
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('level')
+                    ->label("Position Leve")
+                    ->helperText("The smaller is the higger")
+                    ->placeholder("2")
+                    ->required()
+                    ->integer()
+                    ->numeric(),
+                Forms\Components\Textarea::make('description')
+                    ->label("Position Description")
+                    ->helperText("Explain thos posisition")
+                    ->placeholder("Maintenance")
+                    ->rows(5)
+                    ->maxLength(255),
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->placeholder("Descriprion is empty")
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('level')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->emptyStateIcon('heroicon-o-star')
+            ->emptyStateDescription('Create new position data here.')
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make()
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\ForceDeleteAction::make(),
+                Tables\Actions\RestoreAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                //
+            ])
+            ->defaultSort('created_at', 'desc');;
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPositions::route('/'),
+        ];
+    }
+}
