1a463b836dbd5d386affd747ef9dcc2767764c3b
aimanyusufw <aimanyusufdev@gmail.com>
2025-07-06 21:21:58 +0700

    ref : remove default widget

--- app/Providers/Filament/AdminPanelPanelProvider.php
+++ app/Providers/Filament/AdminPanelPanelProvider.php
@@ -39,8 +39,7 @@ class AdminPanelPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                //
             ])
             ->viteTheme('resources/css/filament/admin-panel/theme.css')
             ->middleware([
